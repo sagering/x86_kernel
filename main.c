bbeb74a2dcc9219092046a6eaaa92d0152d7b0f9
anon <anon@anon.org>
2021-04-04 03:53:21 -0700

    Fix page table alignment and initialize interrupts

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 void start(); // has to be first symbol
 
+typedef int i32;
+typedef short int i16;
+typedef char i8;
+
+typedef unsigned int u32;
+typedef unsigned short int u16;
+typedef unsigned char u8;
+
 void put_char(char);
 void print(char const*);
 void clear_screen();
 
 void init_paging();
+void init_interrupt_handlers();
+
+i32 trigger_interrupt();
 
 void start() 
 {
   clear_screen();
-  print("Initializing paging...\n");
+
+  print("Init interrupts...\n");
+  init_interrupt_handlers();
+  print("Interrupts initialized!\n");
+
+  print("Init paging...\n");
   init_paging();
   print("Paging initialized!\n");
 }
-
-typedef unsigned int  u32;
-typedef unsigned char u8;
 
 extern u32 _bss_end;
 
@@ -51,9 +64,10 @@ void memset(u8* dst, u8 val, u32 size)
 typedef u32 PageDirectoryEntry; // PDE
 typedef u32 PageTableEntry;     // PTE
 
-// page directory needs to be 4096 byte aligned
+// page directory needs to be 4096 byte aligned, because only 20 bits in cr3 are used to address the page directory
 PageDirectoryEntry page_dir[PD_NUM_ENTRIES] __attribute__((aligned(PAGE_SIZE)));
-PageTableEntry     page_table_entries[PD_NUM_ENTRIES * PT_NUM_ENTRIES];
+// page table needs to be 4096 byte aligned, because only 20 bits in page directory entry are used to address the page table
+PageTableEntry     page_table_entries[PD_NUM_ENTRIES * PT_NUM_ENTRIES] __attribute__((aligned(PAGE_SIZE)));;
 
 u32 round_down(u32 val, u32 low)
 {
@@ -63,7 +77,7 @@ u32 round_down(u32 val, u32 low)
 void enable_paging()
 {
   asm("mov %cr0, %eax");
-  asm("or $0x80000000, %eax");
+  asm("or $0x80000000, %eax"); // set bit 31 of cr0 to enable paging
   asm("mov %eax, %cr0");
 }
 
@@ -74,30 +88,103 @@ void set_cr3(u32 val)
   asm("mov %eax, %cr3");
 }
 
+void print_u32(u32 val)
+{
+  char buffer[10];
+  i32 i = 0;
+
+  while(val > 0) 
+  {
+    buffer[i++] = '0' + (char)(val % 10);
+    val /= 10;
+  }
+
+  buffer[i++] = '0' + (char)(val % 10);
+
+  while (i--)
+  {
+    put_char(buffer[i]);
+  }
+    
+  put_char('\n');
+}
+
 void init_paging()
 {
+  // set page table for each page direcory entry 
   for(u32 i = 0; i < PD_NUM_ENTRIES; ++i)
   {
     page_dir[i] = (u32) &page_table_entries[i * PT_NUM_ENTRIES];
   }
 
-  u32 from = 0;
   u32 to = round_down((u32)&_bss_end, PAGE_SIZE) + PAGE_SIZE; // exclusive
 
-  for(u32 i = from; i < to; i += PAGE_SIZE)
+  for(u32 i = 0; i < to; i += PAGE_SIZE)
   {
     u32 pdIdx = (i >> 22) & 0x3ff;
     u32 ptIdx = (i >> 12) & 0x3ff;
 
     u32 *pde = &page_dir[pdIdx];
-    *pde |= PDE_PRESENT;
     u32 *pt = (u32*) (*pde & 0xfffff000);
     u32 *pte = &pt[ptIdx];
+
+    *pde |= PDE_PRESENT;
     *pte = i | PTE_PRESENT | PTE_WRITEABLE;
   }
 
   set_cr3( (u32) &page_dir );
-  enable_paging( (u32) &page_dir );
+  enable_paging();
+}
+
+struct InterruptDescriptor
+{
+  u16 offset0;
+  u16 selector;
+  u16 flags;
+  u16 offset1;
+};
+
+// interrupt descriptor table register
+struct IDTR
+{
+  u16 limit;
+  u32 base;
+} __attribute__((packed));
+
+#define NUM_INTERRUPT_DESCRIPTORS 256
+struct InterruptDescriptor interrupt_descriptor_table[NUM_INTERRUPT_DESCRIPTORS];
+struct IDTR idtr;
+
+struct ir_frame;
+__attribute__((interrupt)) void default_interrupt_handler(struct ir_frame* f)
+{
+  print("Default interrupt handler.");
+  while(1);
+}
+
+void load_idt(u32 pIDTR)
+{
+  asm("mov 8(%esp), %eax");
+  asm("lidt (%eax)");
+}
+
+void init_interrupt_handlers()
+{
+  for(int i = 0; i < NUM_INTERRUPT_DESCRIPTORS; ++i)
+  {
+    struct InterruptDescriptor* id = &interrupt_descriptor_table[i];
+    u32 ir = (u32) default_interrupt_handler;
+
+    id->selector = 0x0008;
+    id->offset0  = 0x0000ffff & ir;
+    id->offset1  = 0xffff0000 & ir;
+    id->flags    = 0x8e00;
+  }
+
+  idtr.base = (u32) &interrupt_descriptor_table;
+  idtr.limit = sizeof(struct InterruptDescriptor) * NUM_INTERRUPT_DESCRIPTORS - 1;
+
+  load_idt((u32) &idtr);
 }
 
 char* base = (char*) (0xb8000);
@@ -109,6 +196,13 @@ int current_col = 0;
 
 void put_char(char c) 
 {
+  if(c == '\n')
+  {
+    current_col = 0;
+    ++current_row;
+    return;
+  }
+
   *(base + (current_row * NUM_COLS + current_col) * 2 + 0) = c;
   *(base + (current_row * NUM_COLS + current_col) * 2 + 1) = 128;
 
@@ -125,19 +219,13 @@ void print(char const* s)
 {
   while(*s != 0)
   {
-    if(*s == '\n')
-    {
-      current_col = 0;
-      ++current_row;
-      ++s;
-      continue;
-    }
     put_char(*s);
     ++s;
   }
 }
 
-void clear_screen() {
+void clear_screen()
+{
   char* ptr = (char*) (0xb8000);
 
   for(int r = 0; r < NUM_ROWS; ++r) 
--- makefile
+++ makefile
@@ -2,13 +2,13 @@ bootloader.bin: bootloader.asm
 	nasm -f bin $< -o $@
 
 main.bin: main.c linker.lds
-	gcc -c -m32 -nostdlib -nodefaultlibs -fno-exceptions -static main.c -fno-pie -fno-builtin
+	gcc -c -m32 -nostdlib -nodefaultlibs -fno-exceptions -static main.c -fno-pie -fno-builtin -mgeneral-regs-only
 	ld -melf_i386 -o main.bin -T linker.lds main.o
 
 bootdisk.img: bootloader.bin main.bin
-	dd if=/dev/zero of=bootdisk.img bs=512 count=3
+	dd if=/dev/zero of=bootdisk.img bs=512 count=4
 	dd conv=notrunc if=bootloader.bin of=bootdisk.img bs=512 seek=0 count=1
-	dd conv=notrunc if=main.bin of=bootdisk.img bs=512 seek=1 count=2
+	dd conv=notrunc if=main.bin of=bootdisk.img bs=512 seek=1 count=3
 
 run: bootdisk.img
 	qemu-system-i386 -machine q35 -fda bootdisk.img
